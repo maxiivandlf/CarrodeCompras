d74672e52d4904bd9eee19d6d5a6cdf3d960cee5
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2022-04-10 13:44:24 -0300

    cambio de directorio

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import '../app/App.css';
+// import Login from '../register/login/Login.js';
+// import Signup from '../register/signup/signup';
+import Appp from '../../Prueba_compts/funComp/App2';
+
+function App() {
+  return (
+    <div className='App'>
+      <Appp />
+    </div>
+  );
+}
+
+export default App;
